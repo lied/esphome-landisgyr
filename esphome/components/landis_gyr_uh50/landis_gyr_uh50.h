7a4a28b48e915296bd9563c862f4c2c928a9b7da
lied <lied@gnw9.de>
2025-02-22 17:48:19 +0100

    asd

--- esphome/components/landis_gyr_uh50/landis_gyr_uh50.h
+++ esphome/components/landis_gyr_uh50/landis_gyr_uh50.h
@@ -2,8 +2,8 @@
 #define LANDIS_GYR_UH50_H
 
 #include "esphome.h"
-#include "sensor.h"                         // Works in your environment
-#include "esphome/components/uart/uart.h"   // For uart::UARTComponent and uart::UARTDevice
+#include "esphome/components/sensor/sensor.h"  // Adjusted to match /esphome/esphome/components/sensor/sensor.h
+#include "esphome/components/uart/uart.h"      // For uart::UARTComponent and uart::UARTDevice
 
 namespace esphome {
 namespace landis_gyr_uh50 {
@@ -16,17 +16,16 @@ class LandisGyrUH50 : public Component, public uart::UARTDevice {
   void setup() override;
   void loop() override;
 
-  // Use sensor::Sensor directly, assuming sensor.h provides it
-  void set_energy_sensor(sensor::Sensor *sensor) { energy_sensor_ = sensor; }
-  void set_volume_sensor(sensor::Sensor *sensor) { volume_sensor_ = sensor; }
-  void set_temp_in_sensor(sensor::Sensor *sensor) { temp_in_sensor_ = sensor; }
-  void set_temp_out_sensor(sensor::Sensor *sensor) { temp_out_sensor_ = sensor; }
+  void set_energy_sensor(esphome::sensor::Sensor *sensor) { energy_sensor_ = sensor; }
+  void set_volume_sensor(esphome::sensor::Sensor *sensor) { volume_sensor_ = sensor; }
+  void set_temp_in_sensor(esphome::sensor::Sensor *sensor) { temp_in_sensor_ = sensor; }
+  void set_temp_out_sensor(esphome::sensor::Sensor *sensor) { temp_out_sensor_ = sensor; }
 
  protected:
-  sensor::Sensor *energy_sensor_{nullptr};
-  sensor::Sensor *volume_sensor_{nullptr};
-  sensor::Sensor *temp_in_sensor_{nullptr};
-  sensor::Sensor *temp_out_sensor_{nullptr};
+  esphome::sensor::Sensor *energy_sensor_{nullptr};
+  esphome::sensor::Sensor *volume_sensor_{nullptr};
+  esphome::sensor::Sensor *temp_in_sensor_{nullptr};
+  esphome::sensor::Sensor *temp_out_sensor_{nullptr};
 
   std::vector<uint8_t> buffer_;
   void parse_data_();
