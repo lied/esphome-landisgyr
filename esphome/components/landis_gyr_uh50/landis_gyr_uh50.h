88c5d749e11199a369c650ca7b27cf249593e218
lied <lied@gnw9.de>
2025-02-22 17:11:30 +0100

    asdasd

--- esphome/components/landis_gyr_uh50/landis_gyr_uh50.h
+++ esphome/components/landis_gyr_uh50/landis_gyr_uh50.h
@@ -2,8 +2,8 @@
 #define LANDIS_GYR_UH50_H
 
 #include "esphome.h"
-#include "sensor.h"  // Simplified include for sensor::Sensor
-#include "esphome/components/uart/uart.h"    // Simplified include for UARTComponent and UARTDevice
+#include "sensor.h"                         // For esphome::sensor::Sensor
+#include "esphome/components/uart/uart.h"   // For uart::UARTComponent and uart::UARTDevice
 
 namespace esphome {
 namespace landis_gyr_uh50 {
@@ -16,16 +16,16 @@ class LandisGyrUH50 : public Component, public uart::UARTDevice {
   void setup() override;
   void loop() override;
 
-  void set_energy_sensor(sensor::Sensor *sensor) { energy_sensor_ = sensor; }
-  void set_volume_sensor(sensor::Sensor *sensor) { volume_sensor_ = sensor; }
-  void set_temp_in_sensor(sensor::Sensor *sensor) { temp_in_sensor_ = sensor; }
-  void set_temp_out_sensor(sensor::Sensor *sensor) { temp_out_sensor_ = sensor; }
+  void set_energy_sensor(esphome::sensor::Sensor *sensor) { energy_sensor_ = sensor; }
+  void set_volume_sensor(esphome::sensor::Sensor *sensor) { volume_sensor_ = sensor; }
+  void set_temp_in_sensor(esphome::sensor::Sensor *sensor) { temp_in_sensor_ = sensor; }
+  void set_temp_out_sensor(esphome::sensor::Sensor *sensor) { temp_out_sensor_ = sensor; }
 
  protected:
-  sensor::Sensor *energy_sensor_{nullptr};
-  sensor::Sensor *volume_sensor_{nullptr};
-  sensor::Sensor *temp_in_sensor_{nullptr};
-  sensor::Sensor *temp_out_sensor_{nullptr};
+  esphome::sensor::Sensor *energy_sensor_{nullptr};
+  esphome::sensor::Sensor *volume_sensor_{nullptr};
+  esphome::sensor::Sensor *temp_in_sensor_{nullptr};
+  esphome::sensor::Sensor *temp_out_sensor_{nullptr};
 
   std::vector<uint8_t> buffer_;
   void parse_data_();
